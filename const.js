db2f82518d7bd8a0f53513adece803f6d50370c2
ILYA <106765406+D15ND@users.noreply.github.com>
2022-10-25 21:58:58 +0300

    add

---
+++ const.js
@@ -0,0 +1,10 @@
+const sum=(a,b) => a+b;
+console.log(sum(4,5));
+
+const multNum=(a,b) => a*b;
+console.log(multNum(3,5));
+
+function aezakmi(superSum) {
+    return superSum+superSum;
+}
+console.log(aezakmi(14));
