c91335417194cdb09d939968683427871ca8b9f3
ILYA <106765406+D15ND@users.noreply.github.com>
2022-11-11 20:30:42 +0300

    fix code slice

--- qa.js
+++ qa.js
@@ -1,6 +1,6 @@
 function reversive(val) {
     let revName=val.split('').reverse().join('').toLowerCase();
-    let uppLetter=revName[0].toUpperCase()+revName.slice(1,115);
+    let uppLetter=revName[0].toUpperCase()+revName.slice(1);
     console.log(uppLetter); // alert
 }
 reversive('Hello');
